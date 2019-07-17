c4a1f02f8ee1fe0629397a81e955a1acde537608
sumin416 <sumin416@naver.com>
2019-07-17 15:00:27 -0400

    db-mysql connection code modified

--- src/DBserver/DBconnection.php
+++ src/DBserver/DBconnection.php
@@ -2,14 +2,14 @@
 class Dao
 {
     private $servername = "localhost";
-    private $username = "root";
-    private $password = "";
     private $dbname = "agro";
+    private $username = "root";
+    private $password = "1234";
     private $conn = null;
 
     public function __construct(){
         try{
-            $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=$this->servername;port=3306;dbname=$this->dbname", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }catch(PDOException $e){
             echo "Connection failed:".$e->getMessage();
@@ -20,7 +20,7 @@ class Dao
         return $this->servername;
     }
 
-    // select all datas from inputed table name 
+    // select all datas from inputed table name
     public function select($tname, &$values, $sql= "SELECT * FROM"){
         if($this->conn == null)
             return -1;
@@ -29,7 +29,7 @@ class Dao
         try{
             $result = $this->conn->query($sql);
             $rcount = $result->rowCount();
-            
+
             if($rcount >= 1){
                 $values = $result;
                 return $rcount;
@@ -40,100 +40,42 @@ class Dao
             echo "Select $tname failed:".$e->getMessage();
         }
     }
-
     public function insert($tname, $objects){
         if($this->conn == null)
             return -1;
         // insert row counter
         $count = 0;
 
-        switch($tname){
-            case 'MovieDetail':
-                // prepare and bind to insert moviedetail
-                try{
-                    $stmt = $this->conn->prepare("INSERT INTO MovieDetail(mid, title,
-                    release_date, isAdult, popularity, vote_average, overview, imgsrc)
-                    VALUES (:mid, :title, :release_date, :isAdult, :popularity, :vote_average,
-                    :overview, :imgsrc)");
-                    $stmt->bindParam(':mid', $mid);
-                    $stmt->bindParam(":title", $title);
-                    $stmt->bindParam(":release_date", $release_date);
-                    $stmt->bindParam(":isAdult", $isAdult);
-                    $stmt->bindParam(":popularity", $popularity);
-                    $stmt->bindParam(":vote_average", $vote_average);
-                    $stmt->bindParam(":overview", $overview);
-                    $stmt->bindParam(":imgsrc", $imgsrc);
-
-                    // insert
-                    foreach ($objects as $elem) {
-                        $mid = $elem->mid;
-                        $title = $elem->title;
-                        $release_date = $elem->release_date;
-                        $isAdult = $elem->isAdult;                    
-                        $popularity = $elem->popularity;
-                        $vote_average = $elem->vote_average;
-                        $overview = $elem->overview;
-                        $imgsrc = $elem->imgsrc;
-
-                        $stmt->execute();
-                        $count++;
-                    }
-                }catch(PDOException $e){
-                    echo "MovieDetail: Insert failed:".$e->getMessage();
-                }
-                break;
-
-            case 'Schedule':
-                // prepare and bind to insert schedule
-                try{
-                    $stmt = $this->conn->prepare("INSERT INTO Schedule(mid, time, venue, seatavailable, vsize)
-                    VALUES (:mid, :time, :venue, :seatavailable, :vsize)");
-                    $stmt->bindParam(':mid', $mid);
-                    $stmt->bindParam(":time", $time);
-                    $stmt->bindParam(":venue", $venue);
-                    $stmt->bindParam(":seatavailable", $seatavailable);
-                    $stmt->bindParam(":vsize", $seatavailable);
-
-                    foreach ($objects as $elem) {
-                        $mid = $elem->mid;
-                        $time = $elem->time->format('Y-m-d H:i:s');
-                        $venue = $elem->venue;
-                        $seatavailable = $elem->seatavailable;
-                    
-                        $stmt->execute();
-                        $count++;
-                    }
-                }catch(PDOException $e){
-                    echo "Schedule Insert failed:".$e->getMessage();
-                }
-                break;
-            case 'Seats':
-                // prepare and bind to insert seats
-                try{
-                    $stmt = $this->conn->prepare("INSERT INTO seats(sid, row, col, isavailable)
-                    VALUES (:sid, :row, :col, :isavailable)");
-                    $stmt->bindParam(':sid', $sid);
-                    $stmt->bindParam(":row", $row);
-                    $stmt->bindParam(":col", $col);
-                    $stmt->bindParam(":isavailable", $isavailable);
-
-                    foreach ($objects as $elem) {
-                        $sid = $elem->sid;
-                        $row = $elem->row;
-                        $col = $elem->col;
-                        $isavailable = $elem->isavailable;     
-
-                        $stmt->execute();
-                        $count++;
-                    }
-                }catch(PDOException $e){
-                    echo "Seats: Insert failed:".$e->getMessage();
-                }
-                break;
+        try{
+            $stmt = $this->conn->prepare("INSERT INTO data(id,temperature,date_time,longitude,latitude,satlites,speed)
+            VALUES (:id, :temperature, :date_time, :longitude, :latitude, :satlites, :speed)");
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(":temperature", $temperature);
+            $stmt->bindParam(":date_time", $date_time);
+            $stmt->bindParam(":longitude", $longitude);
+            $stmt->bindParam(":latitude", $latitude);
+            $stmt->bindParam(":satlites", $satlites);
+            $stmt->bindParam(":speed", $speed);
+            // insert
+            foreach ($objects as $elem) {
+                $id = $elem->id;
+                $temperature = $elem->temperature;
+                $date_time = $elem->date_time;
+                $longitude = $elem->longitude;
+                $latitude = $elem->latitude;
+                $satlites = $elem->satlites;
+                $speed = $elem->speed;
+                $stmt->execute();
+                $count++;
+            }
+        }catch(PDOException $e){
+            echo "data table: Insert failed:".$e->getMessage();
         }
-        return $count;            
+
+        }
+        return $count;
     }
-        
+
     public function delete($tname){
         if($this->conn == null)
             return -1;
@@ -141,13 +83,12 @@ class Dao
             $sql = "DELETE FROM ". $tname;
             $result = $this->conn->query($sql);
             $rcount = $result->rowCount();
-
             if($rcount >= 1)    return $rcount;
             else    return $rcount;
         }catch(PDOException $e){
             echo "Delete $tname failed:".$e->getMessage();
         }
     }
-    
+
 }
 ?>
